6fd776c0135646cdb6bce25fb8d3297e1567d71f
baochouu <90813784+baochouu@users.noreply.github.com>
2021-09-16 11:34:09 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,53 +8,53 @@ int main()
     for (i=0;i<=100;i++) {
         system("cls");
         cout <<"\n\t\t\tConnected to Love Server...";
-        cout << "\n\n\t\t\tSend ...::... to my love !";
+        cout << "\n\n\t\t\tSend ...::... to người đọc !";
         cout << "\n\n\t\t__________________________________________";
         cout << "\n\n\n\t\t\t\tLoading   "<<i<<" %"<<endl;
     if (i==10) { cout << "\n\n\t\t\tT            " ;
-                    cout <<"\n\n\n\t\tTO CO DIEU MUON NOI VOI CAU.CAU HAY LANG NGHE NHE!!!   ";
+                    cout <<"\n\n\n\t\tTớ muốn nói với cậu 1 điều, cậu lắng nghe nhé!!!   ";
                     Sleep(4000);          }
                     if (i==20) {cout <<"\n\n\n\t\tLoading....";
                     Sleep(3000); }
     if (i==30) {
-        cout << "\n\n\t\t\tTO                    ";
-        cout << "\n\n\n\t\tNgay tu khi gap cau to da bi cam nang cau mat roi. \n\t\tTo da lo nhu chung ta da tung gap nhau vay!";
+        cout << "\n\n\t\t\tTO  Người người đọc                  ";
+        cout << "\n\n\n\t\tNgay từ lần đầu gặp cậu tớ đã muốn cậu bao tớ trà sữa rồi. \n\t\tTớ đã muốn câu sẽ mua thật nhiều cho tớ!";
         Sleep(4000);
     }
     if (i==40) {
-        cout<< "\n\n\t\t\tTO   Y                  ";
-        cout << "\n\n\n\t\tTu khi gap cau trai tim to luc nao cung loan nhip. \n\t\tHinh nhu cau da danh cap trai tim to roi!";
+        cout<< "\n\n\t\t\tTO   Người đọc               ";
+        cout << "\n\n\n\t\tTớ chỉ thích trà sữa ngon thôi. \n\t\tnên kiếm chổ nào ngon mua cho tớ nhé!";
         Sleep(4000);
     }
     if (i==50) {
-        cout << "\n\n\t\t\tTO   YE               ";
-        cout << "\n\n\n\t\tCau co biet khong? Luc nao to cung nghi ve cau thoi! \n\t\tHinh nhu cau la me tuong lai cua cac con to thi phai.";
+        cout << "\n\n\t\t\tTO   Người đọc           ";
+        cout << "\n\n\n\t\tCậu có biết không? tớ thật sự rất muốn uốn trà sữa \n\t\tRất rất muốn cậu có biết không??.";
         Sleep(5000);
     }
     if (i==60) {
-        cout << "\n\n\t\t\tTO    YEU            ";
-        cout << "\n\n\n\t\tTren the gioi nay co 7 ty nguoi \n\t\tMa sao chi co 1 nguoi yeu cau.";
-        cout << "\n\t\t==>VA NGUOI DO CHINH LA TO DAY !!!";
+        cout << "\n\n\t\t\tTO    Người đọc          ";
+        cout << "\n\n\n\t\tTrên thế giới có 7 tỷ người\n\t\tMà sao tớ chỉ muốn cậu mua trà sữa cho tớ (còn ai mua cho nữa đâu =)).";
+        cout << "\n\t\t==>NGƯỜI ĐỌC LÀ CẬU ĐÓ!!!";
         Sleep(5000);
     }
     if (i==70) {
-        cout << "\n\n\t\t\tTO    YEU    C       ";
-        cout << "\n\n\n\t\tTo la dan ky thuat nen to khong co nhieu van hay,\n\t\t khong lang man nhu nguoi ta. \n\t\tNhung to se bao ve va che cho den het cuoc doi!";
+        cout << "\n\n\t\t\tTO    Người đọc     ";
+        cout << "\n\n\n\t\tTớ tạch văn nên không giỏi màu mè,\n\t\t không vòng vo như người ta \n\t\tNhưng tớ sẽ cho phép cậu mua trà sữa cho tớ hết đời!";
         Sleep(4000);
     }
     if (i==80) {
-        cout << "\n\n\t\t\tTO    YEU    CA       ";
+        cout << "\n\n\t\t\tTO   Người đọc      ";
         cout << "\n\n\n\t\tTo hoc Lap trinh huong doi tuong \n\t\tnhung doi tuong ma to huong den chi co cau ma thoi!  \nS2";
         Sleep(4000);
     }
     if (i==90) {
-        cout << "\n\n\t\t\tTO    YEU    CAU       ";
-        cout << "\n\n\n\t\tHAY LAM NGUOI YEU TO NHE ! TO THICH CAU MAT ROI !  \n\t\tS2";
+        cout << "\n\n\t\t\tTO    Người đọc       ";
+        cout << "\n\n\n\t\tHãy mua trà sữa cho tới nhé ! Tớ chọn cậu rồi !  \n\t\tS2";
         Sleep(5000);
     }
     if (i==98) {
-        cout << "\n\n\t\t\tTO    YEU    CAU   !    ";
-        cout << "\n\n\n\t\tVa To Khong The Song Thieu Cau! Boi Vi To Da Lo Yeu Cau Mat Roi!"<<endl;
+        cout << "\n\n\t\t\tTO    Người đọc   !    ";
+        cout << "\n\n\n\t\tVà tớ không thể sống thiếu trà sữa nhớ mua đem qua cho tớ lúc 7h!"<<endl;
         Sleep(5000);
     }
     if (i==99) {
@@ -177,7 +177,7 @@ int main()
                 cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
                cout <<  "\t\t*     .       *                     *       .       *                 \n";
                 cout << "\t\t*             *                      *              *                   \n";
-                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t*   To        *     Người              *    Đọc   *                    \n";
                 cout << "\t\t  *        *                            *        *                     \n";
                 cout << "\t\t    *    *                                *    *                      \n";
                 cout << "\t\t       *                                     *                         \n";
@@ -187,7 +187,7 @@ int main()
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
-                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t     To             Người                Đọc                           \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
@@ -197,7 +197,7 @@ int main()
                 cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
                cout <<  "\t\t*     .       *                     *       .       *                 \n";
                 cout << "\t\t*             *                      *              *                   \n";
-                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t*   To        *     Người              *    Đọc    *                    \n";
                 cout << "\t\t  *        *                            *        *                     \n";
                 cout << "\t\t    *    *                                *    *                      \n";
                 cout << "\t\t       *                                     *                         \n";
@@ -207,7 +207,7 @@ int main()
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
-                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t     To             Người                 đọc                          \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
@@ -217,7 +217,7 @@ int main()
                 cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
                 cout <<  "\t\t*     .       *                     *       .       *                 \n";
                 cout << "\t\t*              *                     *              *                   \n";
-                cout << "\t\t*   To        *     YEU               *    CAU     *                    \n";
+                cout << "\t\t*   To        *     Người             *    đọc    *                    \n";
                 cout << "\t\t  *        *                            *        *                     \n";
                 cout << "\t\t    *    *                                *    *                      \n";
                 cout << "\t\t       *                                     *                         \n";
@@ -227,7 +227,7 @@ int main()
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
-                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t     To             Người                    Đọc                         \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
                 cout << "\t\t                                                                       \n";
@@ -237,7 +237,7 @@ int main()
                 cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
                 cout <<  "\t\t*     .       *                     *       .       *                 \n";
                 cout << "\t\t*              *                     *              *                   \n";
-                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t*   To        *     ngưới               *    đọc   *                    \n";
                 cout << "\t\t  *        *                            *        *                     \n";
                 cout << "\t\t    *    *                                *    *                      \n";
                 cout << "\t\t       *                                     *                         \n";
@@ -247,12 +247,12 @@ int main()
         Sleep(3000);
     }
     if (i==100) {
-        cout << "\n\n\t\tTo yeu cau.. ::  100% yeu cau!!!";
+        cout << "\n\n\t\tNhớ mua trà sữa nhá.. ::  100% tớ sẽ nhận!!!";
         cout << "\n\n\n\t\t\t------------ MAI MAI YEU CAU --------------\n\n\n\n\n"<<endl;
         Sleep(5000);
     }
     Sleep(100);
     }
-    cout << "\n\n\n\n\t\t\t------Made By Dong Ngo-------    "<< "\n\n\t\t\tCopyright by Dong Ngo   \n\n\n "<<endl;
+    cout << "\n\n\n\n\t\t\t------Made By Chouu-------    "<< "\n\n\t\t\tCopyright by Chouu   \n\n\n "<<endl;
     system("pause") ;
 }
